969b203b5df59d680cce587206fef406886bc04d
Talha-Sajid-Ch <iamtalha.sajid@gmail.com>
2023-01-26 17:04:10 +0500

    Created Application with Styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './todo-list.svg';
 import './App.css';
 
 function App() {
+
+  const [todoText, setTodoText] = React.useState("");
+  const [todoList, setTodoList] = React.useState([]);
+
+  const addList = () => {
+    if (todoText === "") {
+      alert("Please enter a todo item");
+    }
+    else {
+      setTodoList([...todoList, todoText]);
+      setTodoText("");
+    }
+  };
+
+  const clearList = () => {
+    setTodoList([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <div className="center-div">
+        <figure>
+          <img className="logo" src={logo} alt="Todo List"></img>
+        </figure>
+        <div className="caption-div">
+          <h1>Todo List Application</h1>
+        </div>
+        <div className="input-div">
+          <input className="input-field" type="text" placeholder="Add an item..." value={todoText} onChange={(e) => setTodoText(e.target.value)}></input>
+        </div>
+        <div className="button-div">
+          <button className="button" onClick={addList}>Add</button>
+          <button className="button" onClick={clearList}>Clear</button>
+        </div>
+        <div className="list-div">
+          <div className="todo-list">
+            {
+              todoList.map((item) => {
+                return (
+                  <div className="todo-item">
+                    <input type="checkbox" className="checkbox"></input>
+                    <p className="todo-text">{item}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
